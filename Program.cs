f092a99e2ced263165f08bc27fdc8b085d3504f6
Ariel Araujo <a.lucasdearaujo@outlook.com>
2021-07-14 21:34:54 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos ");
+            Console.WriteLine("M - Minuto => 1m = 1 min");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower(); //ToLower => Mudar todas as letras para minúscula.
+            char type = char.Parse(data.Substring(data.Length - 1, 1)); //data.Length => contar quantos caracteres tem na variavel "data".
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000); // Tempo em segundos
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado!!!");
+            Thread.Sleep(2500);
+            Menu();
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
